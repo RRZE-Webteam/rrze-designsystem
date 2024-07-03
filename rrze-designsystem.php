bbd8f9d91a6ffda32904b4c20f68cf0c96e3c249
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-07-03 14:38:49 +0200

    0.0.4 (works! except of HTML on auto-detect)

--- rrze-designsystem.php
+++ rrze-designsystem.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE Designsystem
 Plugin URI:      https://github.com/RRZE-Webteam/rrze-designsystem/
 Description:     Plugin zur Darstellung eines Design Systems in einer WordPress-Instanz  
-Version:         0.0.3
+Version:         0.0.4
 Requires at least: 6.4
 Requires PHP:      8.2
 Author:          RRZE Webteam
